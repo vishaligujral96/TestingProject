dc54c066e11ffa987f098d9989f8bf9871e3b6fe
vishaligujral96 <vishaligujral96@gmail.com>
2020-06-21 15:33:40 +0530

    Maven project

---
+++ src/main/java/ActionClasses.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionClasses {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\work1\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+//		driver.get("https://www.facebook.com/");
+//		
+//		WebElement user =driver.findElement(By.id("email"));
+//		
+//		Actions action =new Actions(driver);
+//		action.moveToElement(user).
+//		click().keyDown(Keys.SHIFT).sendKeys("hello").
+//		keyUp(Keys.SHIFT).doubleClick().contextClick().build().perform();
+		
+//		driver.get("https://www.facebook.com/");
+
+
+	}
+
+}
