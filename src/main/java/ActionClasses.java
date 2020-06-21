6bfc2cbcc3c29884a65b0d7c4e362f5f0e969703
vishaligujral96 <vishaligujral96@gmail.com>
2020-06-21 15:56:32 +0530

    Drag and Drop

--- src/main/java/ActionClasses.java
+++ src/main/java/ActionClasses.java
@@ -1,3 +1,5 @@
+import java.awt.Desktop.Action;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,9 +10,10 @@ import org.openqa.selenium.interactions.Actions;
 public class ActionClasses {
     
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "D:\\work1\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		//EX1 Facebook
 //		driver.get("https://www.facebook.com/");
 //		
 //		WebElement user =driver.findElement(By.id("email"));
@@ -20,7 +23,14 @@ public class ActionClasses {
 //		click().keyDown(Keys.SHIFT).sendKeys("hello").
 //		keyUp(Keys.SHIFT).doubleClick().contextClick().build().perform();
 		
-//		driver.get("https://www.facebook.com/");
+		//EX2 DRAG AND DROP
+		driver.get("http://demo.guru99.com/test/drag_drop.html");
+		WebElement source = driver.findElement(By.id("fourth"));
+		WebElement dest = driver.findElement(By.id("amt7"));
+		
+		Actions action =new Actions(driver);
+		action.dragAndDrop(source, dest).build().perform();
+		
 
 
 	}
